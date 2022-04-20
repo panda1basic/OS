7b3a222012990d8042b91f302ad2c17ace7796e7
Egor <80648346+panda1basic@users.noreply.github.com>
2022-04-20 19:33:53 +0300

    Update OS2.cs

--- OS2.cs
+++ OS2.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Security.Cryptography;
@@ -45,6 +44,86 @@ namespace Pract2
             return hash.ToString();
         }
 
+        static void Theads(string s)
+        {
+            switch (s)
+            {
+                case "1":
+                    Thread threadBruteForcePart1 = new Thread(new ParameterizedThreadStart(NextSequence));
+                    threadBruteForcePart1.Start(new char[] { 'a', 'z', '1' });
+                    break;
+                case "2":
+                    Thread threadBruteForcePart12 = new Thread(new ParameterizedThreadStart(NextSequence));
+                    Thread threadBruteForcePart22 = new Thread(new ParameterizedThreadStart(NextSequence));
+                    threadBruteForcePart12.Start(new char[] { 'a', 'm', '1' });
+                    threadBruteForcePart22.Start(new char[] { 'n', 'z', '2' });
+                    break;
+                case "3":
+                    Thread threadBruteForcePart13 = new Thread(new ParameterizedThreadStart(NextSequence));
+                    Thread threadBruteForcePart23 = new Thread(new ParameterizedThreadStart(NextSequence));
+                    Thread threadBruteForcePart33 = new Thread(new ParameterizedThreadStart(NextSequence));
+                    threadBruteForcePart13.Start(new char[] { 'a', 'i', '1' });
+                    threadBruteForcePart23.Start(new char[] { 'j', 'r', '2' });
+                    threadBruteForcePart33.Start(new char[] { 's', 'z', '3' });
+                    break;
+                case "4":
+                    Thread threadBruteForcePart14 = new Thread(new ParameterizedThreadStart(NextSequence));
+                    Thread threadBruteForcePart24 = new Thread(new ParameterizedThreadStart(NextSequence));
+                    Thread threadBruteForcePart34 = new Thread(new ParameterizedThreadStart(NextSequence));
+                    Thread threadBruteForcePart44 = new Thread(new ParameterizedThreadStart(NextSequence));
+                    threadBruteForcePart14.Start(new char[] { 'a', 'g', '1' });
+                    threadBruteForcePart24.Start(new char[] { 'h', 'n', '2' });
+                    threadBruteForcePart34.Start(new char[] { 'o', 't', '3' });
+                    threadBruteForcePart44.Start(new char[] { 'u', 'z', '4' });
+                    break;
+                case "5":
+                    Thread threadBruteForcePart15 = new Thread(new ParameterizedThreadStart(NextSequence));
+                    Thread threadBruteForcePart25 = new Thread(new ParameterizedThreadStart(NextSequence));
+                    Thread threadBruteForcePart35 = new Thread(new ParameterizedThreadStart(NextSequence));
+                    Thread threadBruteForcePart45 = new Thread(new ParameterizedThreadStart(NextSequence));
+                    Thread threadBruteForcePart55 = new Thread(new ParameterizedThreadStart(NextSequence));
+                    threadBruteForcePart15.Start(new char[] { 'a', 'e', '1' });
+                    threadBruteForcePart25.Start(new char[] { 'f', 'j', '2' });
+                    threadBruteForcePart35.Start(new char[] { 'k', 'o', '3' });
+                    threadBruteForcePart45.Start(new char[] { 'p', 'u', '4' });
+                    threadBruteForcePart55.Start(new char[] { 'v', 'z', '5' });
+                    break;
+                case "6":
+                    Thread threadBruteForcePart16 = new Thread(new ParameterizedThreadStart(NextSequence));
+                    Thread threadBruteForcePart26 = new Thread(new ParameterizedThreadStart(NextSequence));
+                    Thread threadBruteForcePart36 = new Thread(new ParameterizedThreadStart(NextSequence));
+                    Thread threadBruteForcePart46 = new Thread(new ParameterizedThreadStart(NextSequence));
+                    Thread threadBruteForcePart56 = new Thread(new ParameterizedThreadStart(NextSequence));
+                    Thread threadBruteForcePart66 = new Thread(new ParameterizedThreadStart(NextSequence));
+                    threadBruteForcePart16.Start(new char[] { 'a', 'e', '1' });
+                    threadBruteForcePart26.Start(new char[] { 'f', 'j', '2' });
+                    threadBruteForcePart36.Start(new char[] { 'k', 'n', '3' });
+                    threadBruteForcePart46.Start(new char[] { 'o', 'r', '4' });
+                    threadBruteForcePart56.Start(new char[] { 's', 'v', '5' });
+                    threadBruteForcePart66.Start(new char[] { 'w', 'z', '6' });
+                    break;
+                case "7":
+                    Thread threadBruteForcePart17 = new Thread(new ParameterizedThreadStart(NextSequence));
+                    Thread threadBruteForcePart27 = new Thread(new ParameterizedThreadStart(NextSequence));
+                    Thread threadBruteForcePart37 = new Thread(new ParameterizedThreadStart(NextSequence));
+                    Thread threadBruteForcePart47 = new Thread(new ParameterizedThreadStart(NextSequence));
+                    Thread threadBruteForcePart57 = new Thread(new ParameterizedThreadStart(NextSequence));
+                    Thread threadBruteForcePart67 = new Thread(new ParameterizedThreadStart(NextSequence));
+                    Thread threadBruteForcePart77 = new Thread(new ParameterizedThreadStart(NextSequence));
+                    threadBruteForcePart17.Start(new char[] { 'a', 'd', '1' });
+                    threadBruteForcePart27.Start(new char[] { 'e', 'h', '2' });
+                    threadBruteForcePart37.Start(new char[] { 'i', 'l', '3' });
+                    threadBruteForcePart47.Start(new char[] { 'm', 'p', '4' });
+                    threadBruteForcePart57.Start(new char[] { 'q', 't', '5' });
+                    threadBruteForcePart67.Start(new char[] { 'u', 'w', '6' });
+                    threadBruteForcePart77.Start(new char[] { 'x', 'z', '7' });
+                    break;
+                default:
+                    //Console.WriteLine("Неправильный ввод! От 2 до 7 потоков");
+                    break;
+            }
+        }
+
 
         /// Вызов метода для построения хеша на основании пароля. 
         /// Сравнение полученного хеша с заданными.
@@ -58,7 +137,7 @@ namespace Pract2
             {
                 dictPasswords[hash] = password.ToString();
                 nFoundPasswords++;
-                Console.WriteLine("Пароль: " + dictPasswords[hash] + ". Хэш: " + hash + ".");
+                Console.WriteLine("Пароль: " + dictPasswords[hash] + ".\n Хэш: " + hash);
                 return true;
             }
             else
@@ -78,6 +157,7 @@ namespace Pract2
         static void NextSequence(object beginEnd)
         {
             var temp = (char[])beginEnd;
+
             string sequence;
             for (char i1 = temp[0]; i1 <= temp[1]; i1++)
                 foreach (var i2 in alphabet)
@@ -86,15 +166,19 @@ namespace Pract2
                             foreach (var i5 in alphabet)
                             {
                                 sequence = string.Concat(i1, i2, i3, i4, i5);
+                                //Int32 faketimer = (DateTime.Now - dateTimeBegin).Seconds;
+                                int faketimer = (DateTime.Now - dateTimeBegin).Seconds;
                                 if (CompareHash(sequence))
-                                    Console.WriteLine("С запуска потоков и до нахождения пароля прошло " + Math.Round((DateTime.Now - dateTimeBegin).TotalSeconds) + " сек.");
+                                {
+                                    Console.WriteLine("С запуска потоков и до нахождения пароля прошло " + faketimer + " сек.\n");
+                                }
                                 if (nFoundPasswords == nHash)
                                 {
-                                    Console.WriteLine("Поток " + temp[2] + " завершил работу.");
+                                    //Console.WriteLine("Поток " + temp[2] + " завершил работу.");
                                     return;
                                 }
                             }
-            Console.WriteLine("Поток " + temp[2] + " завершил работу.");
+            //Console.WriteLine("Поток " + temp[2] + " завершил работу.");
         }
 
         static void Main(string[] args)
@@ -113,21 +197,24 @@ namespace Pract2
 
             dateTimeBegin = DateTime.Now;
             // Объявление потоков
-            Thread threadBruteForcePart1 = new Thread(new ParameterizedThreadStart(NextSequence));
-            Thread threadBruteForcePart2 = new Thread(new ParameterizedThreadStart(NextSequence));
-            Thread threadBruteForcePart3 = new Thread(new ParameterizedThreadStart(NextSequence));
-            Thread threadBruteForcePart4 = new Thread(new ParameterizedThreadStart(NextSequence));
-            Thread threadBruteForcePart5 = new Thread(new ParameterizedThreadStart(NextSequence));
+
+            ///////Конец//////////
 
             // Запуск потоков
-            threadBruteForcePart1.Start(new char[] { 'a', 'e', '1' });
-            threadBruteForcePart2.Start(new char[] { 'f', 'j', '2' });
-            threadBruteForcePart3.Start(new char[] { 'k', 'o', '3' });
-            threadBruteForcePart4.Start(new char[] { 'p', 'u', '4' });
-            threadBruteForcePart5.Start(new char[] { 'v', 'z', '5' });
-
-            Console.WriteLine("Запущены 5 потоков. Пожалуйста, ожидайте окончания их выполнения...");
-            Console.ReadKey();
+            Console.WriteLine("Введите кол-во потоков: ");
+            string s = Console.ReadLine(); // Ввод с клавиатуры
+            
+            if ((s == "2") | (s == "3") | (s == "4") | (s == "5") | (s == "6") | (s == "7"))
+            {
+                Console.WriteLine("Запущены " + s + " потоков. Пожалуйста, ожидайте окончания их выполнения...\n");
+                Console.ReadKey();
+                Theads(s);
+            }
+            else
+            {
+                Console.WriteLine("Неправильный ввод! От 1 до 7 потоков");
+                Console.ReadKey();
+            }
         }
     }
 }
